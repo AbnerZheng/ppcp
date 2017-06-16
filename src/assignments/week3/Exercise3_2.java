63e52be337c97e98c6a4214a81a5f5f9019623f5
AbnerZheng <abnerzheng@gmail.com>
2017-06-16 17:10:27 +0800

    finish all exercise3.2

---
+++ src/assignments/week3/Exercise3_2.java
@@ -0,0 +1,36 @@
+package assignments.week3;
+
+import sun.jvm.hotspot.utilities.Assert;
+
+import javax.lang.model.type.ArrayType;
+import java.util.Arrays;
+
+/**
+ * Created by abnerzheng on 2017/6/16.
+ */
+public class Exercise3_2 {
+
+    final static int N = 100_000_001;
+
+    public static void main(String[] args) {
+        int[] a = new int[N];
+        Arrays.parallelSetAll(a, index->isPrime(index)?1:0);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(a[i]);
+        }
+        Arrays.parallelPrefix(a, (pre, cur)-> pre + cur);
+        System.out.println(a[N-1]);
+        assert a[N-1] == 664_579;
+
+        for (int i = 0; i < 10; i++) {
+            int i1 = N / 10 * i;
+            System.out.println(a[i1] * Math.log(i1)/ i1);
+        }
+    }
+    private static boolean isPrime(int n) {
+        int k = 2;
+        while (k * k <= n && n % k != 0)
+            k++;
+        return n >= 2 && k * k > n;
+    }
+}
